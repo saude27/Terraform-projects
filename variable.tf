734a5068ce9a9ea75e3d4957cad6dc9ce01243b8
saude27 <akandesaudat2711@gmail.com>
2025-02-18 15:06:49 +0000

    created autoscaling group file

--- variable.tf
+++ variable.tf
@@ -86,3 +86,28 @@ variable "operator_email" {
  description = "a valid email address"
  type = string 
 }
+
+# asg variables
+variable "launch_template_name" {
+ default = "dev-launch-template"
+ description = "name of the launch template"
+ type = string 
+}
+
+variable "ec2_image_id" {
+ default = "ami-08f59be8034cdf23e"
+ description = "id of the ami"
+ type = string 
+}
+
+variable "ec2_instance_type" {
+ default = "t2.micro"
+ description = "the ec2 instance type"
+ type = string 
+}
+
+variable "ec2_key_pair_name" {
+ default = "Testec2keypair"
+ description = "name of the ec2 key pair"
+ type = string 
+}
