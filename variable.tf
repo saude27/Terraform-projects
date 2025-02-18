c6a2134dc891a9bd4421dfa5edeadd7391686110
saude27 <akandesaudat2711@gmail.com>
2025-02-18 12:38:34 +0000

    created rds file

--- variable.tf
+++ variable.tf
@@ -47,3 +47,28 @@ variable "ssh_location" {
  description = "the ip address that can ssh into the ec2 instances"
  type = string 
 }
+
+# rds variables
+variable "database_snapshot_identifier" {
+ default = "arn:aws:rds:eu-west-2:590183879551:snapshot:shopwise"
+ description = "database snapshot arn"
+ type = string 
+}
+
+variable "database_instance_class" {
+ default = "db.t3.micro"
+ description = "the database instance type"
+ type = string 
+}
+
+variable "database_instance_identifier" {
+ default = "dev-rds-db"
+ description = "the database instance identifier"
+ type = string 
+}
+
+variable "multi_az_deployment" {
+ default = false
+ description = "create a standby db instance"
+ type = bool 
+}
